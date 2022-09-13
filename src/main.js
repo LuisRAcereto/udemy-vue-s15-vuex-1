6d63373a99213bb4d9e323cba17df5169feefae2
Luis R. Acereto <lacereto@live.ngcsoftware.com>
2022-09-13 18:01:37 -0400

    Changes from lessons 212 and 213, create a counter usingvuex, to show basic concepts of Global State management with it

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
+// configure store
+// you can only have one store for information.
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+});
 
 const app = createApp(App);
+
+app.use(store);
 
 app.mount('#app');
