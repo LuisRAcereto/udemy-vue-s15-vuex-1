27d68946967129625b392a65889d7e6e51e2fa6a
Luis R. Acereto <lacereto@live.ngcsoftware.com>
2022-09-14 15:37:53 -0400

    Lesson 213 changes, implementing a simple mutation

---
+++ src/components/ChangeCounter.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="addOne">Add 1</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    addOne() {
+      console.log('ChangeCounter Cmp addOne');
+      this.$store.commit('increment');
+    },
+  },
+};
+</script>
