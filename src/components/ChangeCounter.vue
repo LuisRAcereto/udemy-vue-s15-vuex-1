687f544cc9ec8783722df11b212151a145a5212c
Luis R. Acereto <lacereto@live.ngcsoftware.com>
2022-09-14 17:15:59 -0400

    Lesson 219 changes, add mapping helpers for getters and actions

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,14 +1,22 @@
 <template>
-  <button @click="addOne">Add 2</button>
+  <button @click="inc">Add 2</button>
+  <button @click="increase({ value: 11 })">Add 11</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   methods: {
-    addOne() {
-      console.log('ChangeCounter Cmp addOne');
-      this.$store.dispatch('increment');
-    },
+    // addOne() {
+    //   console.log('ChangeCounter Cmp addOne');
+    //   this.$store.dispatch('increment');
+    // },
+    // ...mapActions(['increment', 'incrase']),
+    ...mapActions({
+      inc: 'increment',
+      increase: 'increase',
+    }),
   },
 };
 </script>
